5d2330fb1222f245f5b653e7982da88912ef2d24
gulizseray <gulizseray@gmail.com>
2015-03-27 21:15:39 -0500

    Magnetometer bug fixed

--- app/src/main/java/trackuser/hw2/cs498/illinois/edu/trackuser/MainActivity.java
+++ app/src/main/java/trackuser/hw2/cs498/illinois/edu/trackuser/MainActivity.java
@@ -208,28 +208,29 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
             float deltaT= System.currentTimeMillis() - lastMagnetTime;
 
             //update the magnetometer readings every 50ms
-            if(deltaT > 50) {
-                float[] cacheOld = cachedMagnetometer;
-                cachedMagnetometer = lowPassFilter(sensorEvent.values.clone(), cachedMagnetometer);
+//            if(deltaT > 50) {
+//
+                //cachedMagnetometer = lowPassFilter(sensorEvent.values.clone(), cachedMagnetometer);
 
                 //calculate the angle between phone and north (assuming phone's orientation is parallel to the ground)
 //                float angleNew = (float) Math.atan((double) cachedMagnetometer[0] / cachedMagnetometer[1]) * rToD;
 //
-                float angleNew = (float) Math.atan((double) cachedMagnetometer[0] / cachedMagnetometer[1]);
-
-                //The measurement of this iteration will be the old one in the next iteration
-                angleMag = angleNew;
+                float angleNew = (float) Math.atan2((double) cachedMagnetometer[0], cachedMagnetometer[1]);
 
                 if(angleMagInitial == null)
                     angleMagInitial = new Float(angleMag);
 
+                angleMag = (FILTER_COEFFICIENT) * angleMag + (1 - FILTER_COEFFICIENT) * angleNew;
+
+                //The measurement of this iteration will be the old one in the next iteration
+//                angleMag = angleNew;
 
                 //update display of the angle
                 magMeasurementTextView.setText(String.format("%.3f", Math.toDegrees(angleMag - angleMagInitial)));
 
                 //Set the last update time to the current time. (Might be a good idea to replace it with += deltaT)
                 lastMagnetTime = System.currentTimeMillis();
-            }
+//            }
         }
         else if (mySensor.getType() == Sensor.TYPE_LIGHT){
 
